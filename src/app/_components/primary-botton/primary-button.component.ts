8e2c31db426fc7fe83f4a93411334ad93ffd56e3
igorcauandev <igorcauan036@gmail.com>
2026-02-13 19:52:46 -0300

    Create primary button

---
+++ src/app/_components/primary-botton/primary-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-botton',
+  imports: [],
+  templateUrl: './primary-button.component.html',
+  styleUrl: './primary-button.component.css'
+})
+export class PrimaryButtonComponent {
+
+}
