110aba12ab88920250ed70c596ccb0a67848c5f1
igorcauandev <igorcauan036@gmail.com>
2026-02-14 00:22:51 -0300

    Create and import List Item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './_components/primary-botton/primary-button.component';
 import { SecundaryButtonComponent } from './_components/secundary-button/secundary-button.component';
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent, 
+    PrimaryButtonComponent, 
+    SecundaryButtonComponent,
+    ItemCertificadoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
