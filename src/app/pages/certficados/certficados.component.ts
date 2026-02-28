36e13aba20c4354cf9de1897f20fe40808105347
igorcauandev <igorcauan036@gmail.com>
2026-02-28 17:51:19 -0300

    Create layout screen if existing certificates

--- src/app/pages/certficados/certficados.component.ts
+++ src/app/pages/certficados/certficados.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SecundaryButtonComponent } from '../../_components/secundary-button/secundary-button.component';
+import { ItemCertificadoComponent } from '../../_components/item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-certficados',
   imports: [
-    SecundaryButtonComponent
+    SecundaryButtonComponent,
+    ItemCertificadoComponent
   ],
   templateUrl: './certficados.component.html',
   styleUrl: './certficados.component.css'
