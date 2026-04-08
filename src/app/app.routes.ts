458a55ecc64b2cef61be91930a941e8322194bc8
igorcauandev <igorcauan036@gmail.com>
2026-04-08 17:10:56 -0300

    Cria routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CertficadosComponent } from './pages/certficados/certficados.component';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CertficadosComponent
+  },
+  {
+    path: 'certificados',
+    component: CertficadosComponent
+  },
+  {
+    path: 'certificados/novo',
+    component: CertificadoFormComponent
+  },
+  {
+    path: 'certificado/:id',
+    component: CertificadoComponent
+  }
+];
