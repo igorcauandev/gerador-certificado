343edb9f6fe9c529f1bbced2e6ac025bfe6aa2b0
igorcauandev <igorcauan036@gmail.com>
2026-04-08 17:10:32 -0300

    Adiciona rotas nos botoes

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { SecundaryButtonComponent } from '../secundary-button/secundary-button.component';
+import { RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecundaryButtonComponent],
+  imports: [SecundaryButtonComponent, RouterLink],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
+  id: string = "2";
+  constructor(private router: Router) { }
 
+  redirectToCertificate() {
+    this.router.navigate(['/certificado', 1]);
+  }
 }
--- src/app/_components/secundary-button/secundary-button.component.ts
+++ src/app/_components/secundary-button/secundary-button.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-secundary-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './secundary-button.component.html',
   styleUrl: './secundary-button.component.css'
 })
 export class SecundaryButtonComponent {
+  @Output() click = new EventEmitter<MouseEvent>();
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+
+  handleClick(event: MouseEvent) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.click.emit(event);
+  }
+
   ngOnInit(): void {
     console.log('SecundaryButtonComponent initialized');
   }
