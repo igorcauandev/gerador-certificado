2a3bdcd9086a956477c3217e951f1f29c91a1661
igorcauandev <igorcauan036@gmail.com>
2026-04-15 16:47:08 -0300

    Adiciona validações nos campos de Nome e Atividades

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-botton/primary-button.component';
 import { SecundaryButtonComponent } from '../../_components/secundary-button/secundary-button.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecundaryButtonComponent, PrimaryButtonComponent, FormsModule],
+  imports: [SecundaryButtonComponent, PrimaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
@@ -16,4 +17,8 @@ export class CertificadoFormComponent {
   atividade: string = '';
 
   list_atividades: string[] = [`Atividade 1`, `Atividade 2`, `Atividade 3`];
+
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched;
+  }
 }
