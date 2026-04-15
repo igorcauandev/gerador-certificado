714fe8d1b0191ed0595126535f94a6a17fdf2dd2
igorcauandev <igorcauan036@gmail.com>
2026-04-15 15:54:41 -0300

    Implementa NgModule

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-botton/primary-button.component';
 import { SecundaryButtonComponent } from '../../_components/secundary-button/secundary-button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecundaryButtonComponent, PrimaryButtonComponent],
+  imports: [SecundaryButtonComponent, PrimaryButtonComponent, FormsModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
 
 export class CertificadoFormComponent {
+  nome: string = '';
 
+  atividade: string = '';
+
+  list_atividades: string[] = [`Atividade 1`, `Atividade 2`, `Atividade 3`];
 }
