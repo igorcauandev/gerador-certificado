6a2185834c5191de774fd5288e330580f465d2c9
igorcauandev <igorcauan036@gmail.com>
2026-04-17 16:07:15 -0300

    Ajusta tela de nenhum certificado gerado

--- src/app/pages/certficados/certficados.component.ts
+++ src/app/pages/certficados/certficados.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { ItemCertificadoComponent } from '../../_components/item-certificado/item-certificado.component';
+import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certficados',
   imports: [
-    ItemCertificadoComponent
-  ],
+    ItemCertificadoComponent,
+    SecundaryButtonComponent,
+    RouterLink
+],
   templateUrl: './certficados.component.html',
   styleUrl: './certficados.component.css'
 })
