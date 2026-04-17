98c9df6dd683103996687e5c499180798cf5f8d9
igorcauandev <igorcauan036@gmail.com>
2026-04-17 16:06:08 -0300

    Coloca dados dinamicos e verificação e animação de disabled

--- src/app/_components/primary-botton/primary-button.component.ts
+++ src/app/_components/primary-botton/primary-button.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-botton',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './primary-button.component.html',
   styleUrl: './primary-button.component.css'
 })
 export class PrimaryButtonComponent {
-
+  @Input() text: string = '';
+  @Input() disabled: boolean = false;
 }
