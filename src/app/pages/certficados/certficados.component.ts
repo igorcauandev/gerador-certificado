5bc006d12834ed0df245383ceb847f777bae93ff
igorcauandev <igorcauan036@gmail.com>
2026-04-19 14:34:09 -0300

    Injeta CertificadoService e cria lista de certificados no CertificadosComponent

--- src/app/pages/certficados/certficados.component.ts
+++ src/app/pages/certficados/certficados.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ItemCertificadoComponent } from '../../_components/item-certificado/item-certificado.component';
 import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
 import { RouterLink } from '@angular/router';
+import { CertificadoService } from '../../_services/certificado.service';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certficados',
@@ -13,6 +15,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './certficados.component.html',
   styleUrl: './certficados.component.css'
 })
-export class CertficadosComponent {
+export class CertficadosComponent implements OnInit {
+
+  certificadosList: Certificado[] = [];
+
+  constructor(private certificadoService: CertificadoService) { }
+
+  ngOnInit(): void {
+    this.certificadosList = this.certificadoService.certificados;
+  }
 
 }
